2db649a9d784c3e0e31e640d9ef8dd078cd7fbd0
zheyangdewing <zheyangdewing@MacBook-Pro-2.local>
2023-08-11 19:22:30 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,23 @@
+---
+layout: default
+title: "About"
+---
+## Basic
+---
+性别男，爱代码，爱游戏。
+
+读书跟作者对话，写作想跟读者交流。
+
+## Experience
+---
+- 2012.09 - 2016.07 SXAU.CS.bachelor
+- 2016.03 - 2017.04 Beebank.Developer
+- 2017.04 - 2020.12 Weibo.Developer
+- 2020.12 - Now     Meituan.Developer
+
+## Contact
+---
+- QQ: MTU4OTE0NjA5
+- MAIL: zhenbianshu@foxmail.com
+- 微博：[枕边书_5277](https://weibo.com/u/5715965217)
+- CnBlogs: [枕边书](http://www.cnblogs.com/zhenbianshu/)
---
+++ index.js
@@ -0,0 +1,32 @@
+---
+layout: default
+---
+
+{% for post in paginator.posts %}
+    {% if post.hidden %}
+        {% continue %}
+    {% endif %}
+<div class="article">
+    <div class="well">
+        <h1><a href="{{ site.baseurl}}{{ post.url }}">{{ post.date | date: "%b %-d, %Y" }} - {{ post.title }}</a></h1>
+        <p class="author"><span>{{ post.content | strip_html | strip_newlines | split: "" | size }} Word Count </span></p>
+        <div class="content">
+            {{ post.content | strip_html | truncate: 300 }}
+        </div>
+    </div>
+</div>
+{% endfor %}
+
+<div class="pagination">
+  {% if paginator.previous_page %}
+      {% if paginator.previous_page == 1 %}
+        <a class="btn btn-default" href="{{ site.baseurl}}/index.html" class="previous">Newer</a>
+      {% else %}
+        <a class="btn btn-default" href="{{ site.baseurl}}/page{{ paginator.previous_page }}" class="previous">Newer</a>
+      {% endif %}
+  {% endif %}
+  <span class="page_number ">Page: {{ paginator.page }} of {{ paginator.total_pages }}</span>
+  {% if paginator.next_page %}
+    <a class="btn btn-default" href="{{ site.baseurl}}/page{{ paginator.next_page }}" class="next">Older</a>
+  {% endif %}
+</div>
